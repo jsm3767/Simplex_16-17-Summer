54c20261bf308c6b03e87b1c86f6305e9b0eff5a
jsm3767 <jsm3767@g.rit.edu>
2017-10-02 22:12:57 -0400

    E04 Finished
    I did it in the mesh part

--- 12_MeshClass/AppClass.cpp
+++ 12_MeshClass/AppClass.cpp
@@ -1,6 +1,7 @@
 #include "AppClass.h"
 
 int meshSize = 48;
+float frame = 0;
 void Application::InitVariables(void)
 {
 	////Change this to your name and email
@@ -31,9 +32,15 @@ void Application::Display(void)
 	// Clear the screen
 	ClearScreen();
 
+	frame ++;
+	if (frame > 360) {
+		frame = 0;
+	}
+
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
-	static matrix4 m4Model;
+	static matrix4 m4Model = matrix4() * glm::translate(IDENTITY_M4, vector3(-40, 0, -20));
+	m4Model = glm::translate(m4Model, vector3(glm::abs(glm::sin(glm::radians(frame)))/20, glm::cos(glm::radians(frame))/30, 0));
 
 	for (int i = 0; i < meshSize; i++) {
 		if (i < 2) {
@@ -66,6 +73,18 @@ void Application::Display(void)
 		else if (i < 38) {
 			m_pMesh[i].Render(m4Projection, m4View, m4Model * glm::translate(IDENTITY_M4, vector3((i - 37) + 10, 3, 0)));
 		}
+		else if (i < 40) {
+			m_pMesh[i].Render(m4Projection, m4View, m4Model * glm::translate(IDENTITY_M4, vector3((i - 38) * 2, 2, 0)));
+		}
+		else if (i < 42) {
+			m_pMesh[i].Render(m4Projection, m4View, m4Model * glm::translate(IDENTITY_M4, vector3((i - 40) * 2 + 8, 2, 0)));
+		}
+		else if (i < 44) {
+			m_pMesh[i].Render(m4Projection, m4View, m4Model * glm::translate(IDENTITY_M4, vector3((i - 42) + 3, 1, 0)));
+		}
+		else if (i < 46) {
+			m_pMesh[i].Render(m4Projection, m4View, m4Model * glm::translate(IDENTITY_M4, vector3((i - 44) + 6, 1, 0)));
+		}
 		
 	}
 		
@@ -88,7 +107,7 @@ void Application::Release(void)
 {
 	if (m_pMesh != nullptr)
 	{
-		delete m_pMesh;
+		delete[] m_pMesh;
 		m_pMesh = nullptr;
 	}
 	SafeDelete(m_pMesh1);
